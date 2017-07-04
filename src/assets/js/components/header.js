09a7750c41acfd1428e2a441ce9cb3bc36a0b136
strongwarmi <elianasc10@hotmail.com>
2017-07-04 11:10:25 -0500

    primer paso para conectacon con el api y obtener la url

---
+++ src/assets/js/components/header.js
@@ -0,0 +1,7 @@
+'use strict';
+const Header = () => {
+    const header = $('<header class="center-align"></header>');
+    const titulo = $('<h3 class="red-text text-darken-2">Pokédex</h3>');
+    header.append(titulo);
+    return header;
+}
---
+++ src/assets/js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const render = (root) => {
+    root.empty();
+    // Append components
+    root.append(Header());
+    //root.append(Search());
+};
+
+const state = {
+    pin: null,
+    /*selectedPokemon: null*/
+};
+
+$(_ => {
+
+    getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AQA6SxA52Kr8rZEppfw-TgqsLSA6FM4qoiPYYN1EI1rXoUA7AwAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cmedia%2Cattribution%2Cboard%2Cmetadata%2Ccolor%2Ccounts%2Coriginal_link%2Ccreated_at%2Ccreator%2Cimage', (err, json) => {
+
+        if (err) {
+            return alert(err.message);
+        }
+
+        state.pin = json;
+        console.log(state.pin.data[0].image.original.url);
+
+        const root = $('.root');
+        render(root);
+    });
+
+});

a1db8fb63ce0cd6de64471ae5e291190d6938e15
lesashley <lesashley56@gmail.com>
2017-07-06 05:01:24 -0500

    Agregando en server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const app = express();
-
-app.use('/', express.static('public'));
-
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+// const express = require('express');
+// const app = express();
+//
+// app.use('/', express.static('public'));
+//
+// app.listen(3000, () => {
+//   console.log("Listening on 3000");
+// });
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/'));
+app.set('', __dirname + '/');
+app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.render('index');
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });

16f480e822295d0528990b74a3ab0e9d38ff37b7
LeidyRosmery <rosm3rita@hotmail.com>
2017-07-06 03:24:38 -0500

    informacion pines

--- src/assets/js/components/dashboard.js
+++ src/assets/js/components/dashboard.js
@@ -10,11 +10,47 @@ const Dashboard = () => {
           return alert(err.message);
       }
        state.dashboard=json.data;
-       console.log(state.dashboard);
+
 
        for(var i = 0; i < state.dashboard.length; i++) {
-              state.pinesID.push(state.dashboard[i].id);
-            console.log(state.pinesID.length);
+
+                getJSON("https://api.pinterest.com/v1/pins/" + state.dashboard[i].id+ "/?access_token=AWRwPAI2kL0bx9lF9nybb8GcnDgdFM6gaE1AuhBEI1kRKQBDLwAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cattribution%2Cmetadata%2Ccolor%2Coriginal_link%2Cboard%2Ccreated_at%2Ccounts%2Cimage%2Ccreator%2Cmedia", (err, json)  => {
+                  if (err) {
+                      return alert(err.message);
+                  }
+            console.log(json.data);
+                  state.data = json.data;
+                  console.log(state.data.note);
+                  console.log(state.data.board.name);
+                  console.log(state.data.creator.first_name);
+
+                  if (state.data.attribution !== undefined ) {
+                      console.log(state.data.attribution.title);
+                      console.log(state.data.attribution.provider_name);
+                      console.log(state.data.attribution.provider_favicon_url);
+                      console.log(state.data.attribution.author_name);
+                  }
+                  //a veces de por si esta vacio metadata
+                  if (state.data.metadata.link !== undefined) {
+                      console.log(state.data.metadata.link.favicon);
+                      console.log(state.data.metadata.link.site_name)
+                      console.log(state.data.metadata.link.title);
+                      console.log(state.data.metadata.link.description);
+                  } else {
+                      if (state.data.metadata.product !== undefined) {
+                          console.log(state.data.metadata.product.name);
+
+                      }
+                  }
+                  // a veces en vez de tener article tiene product
+
+                  if (state.data.metadata.article !== undefined) {
+                      console.log(state.data.metadata.article.name);
+                      console.log(state.data.metadata.article.published_at);
+                      console.log(state.data.metadata.article.description);
+                  }
+
+});
            var image = $('<img class="img-tablero" data-id="'+ state.dashboard[i].id+'" src="' + state.dashboard[i].image.original.url + '" alt="">');
 
            var nameUser = $('<span class="">' + state.nameUser + '</span>');
@@ -27,10 +63,10 @@ const Dashboard = () => {
            pin.append(image);
            pin.append(nameUser);
            pin.append(photoUser);
-           console.log(state.pinesID);
+
        }
   });
-console.log(state.pinesID);
+
 
     div.append(pin);
 
